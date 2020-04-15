88da7805a1dfd13d885cc9139e0f258b377e034a
dengshihao <dengshihao@witenet.com>
2020-04-15 10:35:48 +0800

    delete

--- src/utils/index.js
+++ src/utils/index.js
@@ -92,12 +92,3 @@ export function splitUrl(url) {
 }
 
 console.log("maste21313123r")
-
-console.log(111);
-console.log(222);
-console.log(333);
-console.log(444);
-console.log(555);
-console.log(666);
-console.log(777);
-console.log(888);
